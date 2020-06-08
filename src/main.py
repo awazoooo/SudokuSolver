673457494fd47ea9c37588715b633abacaf1b95b
awazoooo <awazo4662@gmail.com>
2020-06-08 22:15:42 +0900

    add Z3 solver

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from z3 import *
+
 class Cell:
     def __init__(self, pos, value, size):
         # Position number
@@ -92,35 +94,43 @@ class Board:
             raise ValueError(err.format(size, size**2, len(cells)))
 
         #self.cells = [Cell(i, v, size) for i, v in enumerate(cells)] # list
-        self.cells = {Cell(i, v, size) for i, v in enumerate(cells)} # set
-        self.size = size
+        self._cells = {Cell(i, v, size) for i, v in enumerate(cells)} # set
+        self._size = size
 
 
     def __repr__(self):
-        return 'Board({:d}x{:d})'.format(self.size, self.size)
+        return 'Board({:d}x{:d})'.format(self._size, self._size)
+
+
+    def get_size(self):
+        return self._size
+
+
+    def get_cells(self):
+        return self._cells
 
 
     def get_col(self, col_num):
         """ Get cells of the column """
         # TODO: setを返すようにする？
         #return self.cells[col_num::size]
-        return filter(lambda c: c.col() == col_num, self.cells)
+        return filter(lambda c: c.col() == col_num, self._cells)
 
 
     def get_row(self, row_num):
         """ Get cells of the row """
         #return self.cells[size*row_num:size*(row_num+1)-1]
-        return filter(lambda c: c.row() == row_num, self.cells)
+        return filter(lambda c: c.row() == row_num, self._cells)
 
 
     def get_block(self, block_num):
         """ Get cells of the block """
-        return filter(lambda c: c.block() == block_num, self.cells)
+        return filter(lambda c: c.block() == block_num, self._cells)
 
 
     def get_cell(self, cell_id):
         """ Get cells of the id """
-        return list(filter(lambda c: c.cell_id() == cell_id, self.cells))[0]
+        return list(filter(lambda c: c.cell_id() == cell_id, self._cells))[0]
 
 
     def valid(self):
@@ -132,11 +142,11 @@ class Board:
             #print(list(filtered), set(filtered))
             return len(filtered) == len(set(filtered))
 
-        return all(check(f(i)) for f in gen_funcs for i in range(self.size))
+        return all(check(f(i)) for f in gen_funcs for i in range(self._size))
 
 
     def fill(self):
-        return all(cell.value() is not None for i in range(self.size) for cell in self.get_row(i))
+        return all(cell.value() is not None for i in range(self._size) for cell in self.get_row(i))
 
 
     def is_solved(self):
@@ -152,7 +162,7 @@ class Board:
         def get_value_list(l):
             return list(map(lambda c: 0 if c.value() is None else c.value(), l))
 
-        for i in range(self.size):
+        for i in range(self._size):
             l = sorted(self.get_row(i), key=lambda c: c.cell_id())
             print(get_value_list(l))
 
@@ -175,7 +185,7 @@ class Board:
         """ Update all cells candidates """
         update = False
         while True:
-            for cell in self.cells:
+            for cell in self._cells:
                 if self.update_one_cell(cell):
                     update = True
                     print('Update {}'.format(cell))
@@ -186,5 +196,70 @@ class Board:
             update = False
 
 
-l = [int(i) for i in list('000009806306810000080002070030070402070604050502080010020100060000095308804700000')]
-b = Board(l, 9)
+
+class Z3Solver:
+    def __init__(self, board):
+        self._board = board
+        self._solver = Solver()
+        self._size = board.get_size()
+        self._vals = \
+            [Int('cell_{:d}'.format(i)) for i in range(int(board.get_size()**2))]
+
+
+    def cell_value_restrict(self):
+        """ All cell have a value between 1 to {size} """
+        for val in self._vals:
+            self._solver.add(1 <= val, val <= self._size)
+
+
+    def add_unique_restrict(self, l):
+        self._solver.add(Distinct(l))
+
+
+    def row_unique_restrict(self):
+        """ Distinct cell values in the same row """
+        for i in range(self._size):
+            self.add_unique_restrict(self._vals[self._size*i:self._size*(i+1)])
+
+
+    def col_unique_restrict(self):
+        """ Distinct cell values in the same col """
+        for i in range(self._size):
+            self.add_unique_restrict(self._vals[i::self._size])
+
+
+    def block_unique_restrict(self):
+        """ Distinct cell values in the same block """
+        for i in range(self._size):
+            target_cells = map(lambda c: c.cell_id(), self._board.get_block(i))
+            # TODO: 一応リストにしてるけどいてらぶるでも良いかも
+            self.add_unique_restrict([self._vals[i] for i in target_cells])
+
+
+    def init_values(self):
+        """ Add initial value restricts """
+        for cell in filter(lambda c: c.value() is not None, self._board.get_cells()):
+            self._solver.add(self._vals[cell.cell_id()] == cell.value())
+
+
+    def solve(self):
+        self.cell_value_restrict()
+        self.row_unique_restrict()
+        self.col_unique_restrict()
+        self.block_unique_restrict()
+        self.init_values()
+
+        if self._solver.check() == sat:
+            print('Solved sudoku!')
+            m = self._solver.model()
+            for i in range(self._size):
+                print([m[self._vals[i*self._size+j]] for j in range(self._size)])
+
+        else:
+            print('Can\'t solve...')
+
+
+if __name__ == '__main__':
+    l = [int(i) for i in list('000009806306810000080002070030070402070604050502080010020100060000095308804700000')]
+    b = Board(l, 9)
+    solver = Z3Solver(b)
