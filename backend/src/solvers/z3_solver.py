4dae42d0d60d2fadd9c98224c4100329357f73bf
awazoooo <awazo4662@gmail.com>
2020-06-18 23:11:31 +0900

    add: z3_solver test

--- backend/src/solvers/z3_solver.py
+++ backend/src/solvers/z3_solver.py
@@ -9,30 +9,25 @@ class Z3Solver:
             [Int('cell_{:d}'.format(i)) for i in range(board.size**2)]
 
 
-    def cell_value_restrict(self):
+    def _cell_value_restrict(self):
         """ All cell have a value between 1 to {size} """
         for val in self.z3_vars:
             self.solver.add(1 <= val, val <= self.size)
 
 
-    def add_unique_restrict(self, l):
-        self.solver.add(Distinct(l))
-
-
-    def row_unique_restrict(self):
+    def _row_unique_restrict(self):
         """ Distinct cell values in the same row """
         for i in range(self.size):
             self.solver.add(Distinct([self.z3_vars[c.cell_id] for c in self.board.get_row(i)]))
 
 
-    def col_unique_restrict(self):
+    def _col_unique_restrict(self):
         """ Distinct cell values in the same col """
         for i in range(self.size):
             self.solver.add(Distinct([self.z3_vars[c.cell_id] for c in self.board.get_col(i)]))
-            #self.add_unique_restrict(self.vals[i::self.size])
 
 
-    def block_unique_restrict(self):
+    def _block_unique_restrict(self):
         """ Distinct cell values in the same block """
         for i in range(self.size):
             self.solver.add(Distinct([self.z3_vars[c.cell_id] for c in self.board.get_block(i)]))
@@ -41,18 +36,18 @@ class Z3Solver:
             #self.add_unique_restrict([self._vals[i] for i in target_cells])
 
 
-    def init_values(self):
+    def _init_values(self):
         """ Add initial value restricts """
         for cell in filter(lambda c: c.value, self.board):
             self.solver.add(self.z3_vars[cell.cell_id] == cell.value)
 
 
     def solve(self):
-        self.cell_value_restrict()
-        self.row_unique_restrict()
-        self.col_unique_restrict()
-        self.block_unique_restrict()
-        self.init_values()
+        self._cell_value_restrict()
+        self._row_unique_restrict()
+        self._col_unique_restrict()
+        self._block_unique_restrict()
+        self._init_values()
 
         if self.solver.check() == sat:
             print('Solved sudoku!')
